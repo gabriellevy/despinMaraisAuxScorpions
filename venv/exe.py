8657ee2452cc9507475e25d0128beb3b963f31d2
Mathieu <mdeharbe@hotmail.com>
2020-07-25 21:01:31 +0200

    structure de base + pdf des ldoelh

--- venv/exe.py
+++ venv/exe.py
@@ -1,11 +1,6 @@
-from gen.gen_histoire import *
+from genMaraisAuxScorpions import GenMaraisAuxScorpions
 
-# stupides tests
+# exécution de l'histoire
 print("------ Création de 'Le Marais aux scorpions'")
-truc = GenHist("Le Marais aux scorpions")
-evt1 = truc.AjouterEvt("id1");
-evt2 = truc.AjouterEvt("id2");
-effet1 = truc.AjouterEffet("Il se passe des choses dans cet effet d'histoire ! ", evt = evt2)
-effet2 = truc.AjouterEffet("Mais vraiment plein de trucs ", evt = evt2)
+truc = GenMaraisAuxScorpions()
 print(truc._m_Histoire)
-print(truc._m_Histoire.__contains__(evt1))
